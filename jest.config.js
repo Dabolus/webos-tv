7faf936a2fd55560707d347d89a70d8494636d30
Giorgio Garasto <giorgio@garasto.me>
2022-11-29 22:58:28 +0100

    test: update jest test coverage thresholds to 90%

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ export default {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
   projects: [
